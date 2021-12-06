d7795c84f792ab83e75c7ab1ca996934c7deaae7
Michelle Yang <53619397+micya@users.noreply.github.com>
2021-12-06 20:44:40 +0000

    added solution for day 6

---
+++ day-6/lanternfish-1.py
@@ -0,0 +1,26 @@
+counts = dict()
+days = 80
+
+with open("input.txt") as file:
+    ages = file.readline().split(",")
+
+for i in range(9):
+    counts[i] = 0
+
+for age in ages:
+    counts[int(age)] += 1
+
+for _ in range(days):
+    new_fish = counts[0]
+
+    for i in range(8):
+        counts[i] = counts[i + 1]
+    
+    counts[6] += new_fish
+    counts[8] = new_fish
+
+total = 0
+for age in counts:
+    total += counts[age]
+
+print(total)
