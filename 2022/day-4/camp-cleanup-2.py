b24be72fe5b7774be877a5d2d9c79ec514d912e4
Michelle Yang <michelle_mit@hotmail.com>
2022-12-17 04:36:33 +0000

    finished day 4

---
+++ 2022/day-4/camp-cleanup-2.py
@@ -0,0 +1,25 @@
+class Section:
+    def __init__(self, section: str) -> None:
+        parts = section.strip().split('-')
+        self.start = int(parts[0])
+        self.end = int(parts[1])
+
+    def contains(self, other: "Section") -> bool:
+        return self.start <= other.start and self.end >= other.end
+
+    def overlaps(self, other: "Section") -> bool:
+        return self.end >= other.start and self.start <= other.end
+
+
+count = 0
+
+for line in open("input.txt"):
+    line = line.strip()
+    parts = line.split(',')
+    section1 = Section(parts[0])
+    section2 = Section(parts[1])
+
+    if section1.overlaps(section2):
+        count += 1
+
+print(count)
